69ce41d77fcff68764e474646cc4da3b85b50d0a
Egor Kozlov <egor.kozlov@rsquad.io>
2022-11-23 22:57:05 +0300

    update ape model

--- src/models/ape.model.ts
+++ src/models/ape.model.ts
@@ -1,10 +1,10 @@
-import {model, property, hasMany, hasOne} from '@loopback/repository';
+import {model, property, hasMany, hasOne, Entity} from '@loopback/repository';
 import {NftItem} from '.';
 import {Sales} from './sales.model';
 import {Listing} from './listing.model';
 
 @model()
-export class Ape extends NftItem {
+export class Ape extends Entity {
   @property({
     type: 'number',
     id: true,
@@ -14,45 +14,87 @@ export class Ape extends NftItem {
 
   @property({
     type: 'number',
+    default: '0'
+  })
+  tokenId?: number;
+
+  @property({
+    type: 'string',
     default: ''
+  })
+  contractAddress?: string;
+
+  @property({
+    type: 'string',
+    default: ''
+  })
+  name?: string;
+
+  @property({
+    type: 'string',
+    default: ''
+  })
+  description?: string;
+
+  @property({
+    type: 'string',
+    default: ''
+  })
+  image?: string;
+
+  @property({
+    type: 'number',
+    default: 0
   })
   rarity?: number;
 
   @property({
     type: 'number',
-    default: ''
+    default: 0
   })
   mask?: number;
 
   @property({
     type: 'number',
-    default: ''
+    default: 0
   })
   hat?: number;
 
   @property({
     type: 'number',
-    default: ''
+    default: 0
   })
   glasses?: number;
 
   @property({
     type: 'number',
-    default: ''
+    default: 0
   })
   jewerly?: number;
 
   @property({
     type: 'number',
-    default: ''
+    default: 0
   })
   clothes?: number;
 
   @property({
     type: 'number',
-    default: ''
+    default: 0
   })
   tatoos?: number;
+
+  @property({
+    type: 'boolean',
+    default: 'false'
+  })
+  onSale?: boolean;
+
+  @property({
+    type: 'boolean',
+    default: 'false'
+  })
+  onAuction?: boolean;
 
   @hasMany(() => Sales)
   sales: Sales[];
